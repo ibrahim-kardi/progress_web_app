253660c3ccad3a886763d0f0eb92799836ab7fd7
unknown <ikardi04@gmail.com>
2018-11-23 02:54:56 +0600

    first commit

---
+++ workbox-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "img/**.*"
+  ],
+  "swSrc": "src/sw.js",
+  "swDest": "sw.js",
+  "globIgnores": [
+    "./workbox-config.js"
+  ]
+};
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "img/**.*",
+    "offline.html"
+  ],
+  "swSrc": "src/sw.js",
+  "swDest": "sw.js",
+  "globIgnores": [
+    "./workbox-config.js"
+  ]
+};
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "img/**.*",
+    "offline.html",
+    "icons/**.*"
+  ],
+  "swSrc": "src/sw.js",
+  "swDest": "sw.js",
+  "globIgnores": [
+    "./workbox-config.js"
+  ]
+};
